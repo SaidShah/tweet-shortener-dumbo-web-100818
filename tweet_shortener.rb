f7fb27457bd503fdacc4333755cb6c8a20646fd9
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-23 01:24:02 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,5 +4,11 @@ def dictionary
   word_list = {
     "hello" => "hi",
     "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
