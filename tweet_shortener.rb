0a72628ee793738cd35ca7844f53b8b0d5b1c756
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-23 01:50:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,7 @@ def word_substituter(given_string)
   my_dictionary = dictionary
   my_dictionary.each do |word, sub_word|
   word_to_sub = /\b#{word}\b/
- given_string.gsub!(word_to_sub,sub_word)
+ given_string.gsub!(/\b#{word}\b/,sub_word)
   end
   given_string
 end
